04be18310ef522e044357208ed89b31bb3ef0a4f
yemimaclra <yemimanainggolan0@gmail.com>
2024-09-13 17:59:14 +0700

    Menyelesaikan tutorial 2!!

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://yemima-clara31-mentalhealthtracker.pbp.cs.ui.ac.id/", "https://yemima-clara31-mentalhealthtracker.pbp.cs.ui.ac.id/"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1", "https://yemima-clara31-mentalhealthtracker.pbp.cs.ui.ac.id/", "http://yemima-clara31-mentalhealthtracker.pbp.cs.ui.ac.id/"]
